9eabdf859e3b6e729bbd4ec1a825d928ba0359a0
Joshua Gian <56461377+joshgian@users.noreply.github.com>
2024-07-01 21:34:19 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from tensorflow.keras.models import load_model
+import pickle
+
+# Judul aplikasi
+st.title('Sistem Prediksi Umur Harapan Hidup')
+
+# Input dari pengguna
+feature1 = st.number_input('Feature1', min_value=0.0, max_value=100.0)
+feature2 = st.number_input('Feature2', min_value=0.0, max_value=100.0)
+category = st.selectbox('Category', ['A', 'B', 'C'])
+
+# Prediksi ketika tombol diklik
+if st.button('Predict'):
+    # Muat model, encoder, dan scaler dari file
+    model = load_model('model_ann.h5')
+
+    with open('label_encoder_.pkl', 'rb') as encoder_file:
+        label_encoder = pickle.load(encoder_file)
+
+    with open('scaler.pkl', 'rb') as scaler_file:
+        scaler = pickle.load(scaler_file)
+
+    # Transform input dari pengguna
+    input_data = pd.DataFrame([[category, feature1, feature2]], columns=['Category', 'Feature1', 'Feature2'])
+    input_data['Category'] = label_encoder.transform(input_data['Category'])
+    input_data[['Feature1', 'Feature2']] = scaler.transform(input_data[['Feature1', 'Feature2']])
+
+    # Prediksi
+    prediction = model.predict(input_data)
+    st.write('Predicted Life Expectancy:', prediction[0][0])

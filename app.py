2873de87923698f89f26da7f4215152490e333fb
Joshua Gian <56461377+joshgian@users.noreply.github.com>
2024-07-01 23:44:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from tensorflow.keras.models import load_model
 import pickle
 
 
-st.title('All the Features you need to input to start the Prediction' , style={'font-size': '24px', 'color': 'blue'})
+st.title('All the Features you need to input to start the Prediction')
 image_url = 'https://raw.githubusercontent.com/joshgian/Life-Expectancy-Regression/main/All%20Features.png'
 
 # Menampilkan gambar dari URL

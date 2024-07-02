d069fa2bca4ebd63ea1bcb549cdfd125bd08eabc
Joshua Gian <56461377+joshgian@users.noreply.github.com>
2024-07-02 10:48:51 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -18,11 +18,62 @@ st.title('Example Value to input')
 df = pd.read_csv('Life Expectancy Data.csv')
 df
 
+
+
+# Daftar negara
+countries = np.array(['Afghanistan', 'Albania', 'Algeria', 'Angola',
+       'Antigua and Barbuda', 'Argentina', 'Armenia', 'Australia',
+       'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh',
+       'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan',
+       'Bolivia (Plurinational State of)', 'Bosnia and Herzegovina',
+       'Botswana', 'Brazil', 'Brunei Darussalam', 'Bulgaria',
+       'Burkina Faso', 'Burundi', "Côte d'Ivoire", 'Cabo Verde',
+       'Cambodia', 'Cameroon', 'Canada', 'Central African Republic',
+       'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo',
+       'Cook Islands', 'Costa Rica', 'Croatia', 'Cuba', 'Cyprus',
+       'Czechia', "Democratic People's Republic of Korea",
+       'Democratic Republic of the Congo', 'Denmark', 'Djibouti',
+       'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt',
+       'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia',
+       'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia',
+       'Georgia', 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala',
+       'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras',
+       'Hungary', 'Iceland', 'India', 'Indonesia',
+       'Iran (Islamic Republic of)', 'Iraq', 'Ireland', 'Israel', 'Italy',
+       'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati',
+       'Kuwait', 'Kyrgyzstan', "Lao People's Democratic Republic",
+       'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Lithuania',
+       'Luxembourg', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives',
+       'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius',
+       'Mexico', 'Micronesia (Federated States of)', 'Monaco', 'Mongolia',
+       'Montenegro', 'Morocco', 'Mozambique', 'Myanmar', 'Namibia',
+       'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua',
+       'Niger', 'Nigeria', 'Niue', 'Norway', 'Oman', 'Pakistan', 'Palau',
+       'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines',
+       'Poland', 'Portugal', 'Qatar', 'Republic of Korea',
+       'Republic of Moldova', 'Romania', 'Russian Federation', 'Rwanda',
+       'Saint Kitts and Nevis', 'Saint Lucia',
+       'Saint Vincent and the Grenadines', 'Samoa', 'San Marino',
+       'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia',
+       'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia',
+       'Solomon Islands', 'Somalia', 'South Africa', 'South Sudan',
+       'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden',
+       'Switzerland', 'Syrian Arab Republic', 'Tajikistan', 'Thailand',
+       'The former Yugoslav republic of Macedonia', 'Timor-Leste', 'Togo',
+       'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey',
+       'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine',
+       'United Arab Emirates',
+       'United Kingdom of Great Britain and Northern Ireland',
+       'United Republic of Tanzania', 'United States of America',
+       'Uruguay', 'Uzbekistan', 'Vanuatu',
+       'Venezuela (Bolivarian Republic of)', 'Viet Nam', 'Yemen',
+       'Zambia', 'Zimbabwe'])
+
 # Judul aplikasi
 st.title('Life Expectancy Prediction tools')
 
 # Input dari pengguna
-country = st.selectbox('Country', ['Country1', 'Country2', 'Country3'])  # Sesuaikan dengan daftar negara yang tersedia
+country = st.selectbox('Country', countries)
 year = st.slider('Year', min_value=2000, max_value=2015)
 status = st.selectbox('Status', ['Developing', 'Developed'])
 adult_mortality = st.number_input('Adult Mortality', min_value=0, max_value=1000)
